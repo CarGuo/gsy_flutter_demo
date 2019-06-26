742a1209ec7709f52264bba7c1eec595285cff9b
guoshuyu <359369982@qq.com>
2019-06-26 11:26:04 +0800

    富文本显示

--- lib/widget/rich_text_demo_page.dart
+++ lib/widget/rich_text_demo_page.dart
@@ -9,70 +9,86 @@ class RichTextDemoPage extends StatefulWidget {
 
 class _RichTextDemoState extends State<RichTextDemoPage> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext mainContext) {
     return Scaffold(
       appBar: AppBar(
         title: new Text("RichTextDemoPage"),
       ),
       body: new Container(
         margin: EdgeInsets.all(10),
-        child: Center(
-          child: RealRichText([
-            TextSpan(
-              text: "A Text Link",
-              style: TextStyle(color: Colors.red, fontSize: 14),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () {
-                  debugPrint("Link Clicked.");
-                },
-            ),
-            ImageSpan(
-              AssetImage("static/gsy_cat.png"),
-              imageWidth: 24,
-              imageHeight: 24,
-            ),
-            ImageSpan(AssetImage("static/gsy_cat.png"),
+        child: Builder(builder: (context) {
+          return Center(
+            child: RealRichText([
+              TextSpan(
+                text: "A Text Link",
+                style: TextStyle(color: Colors.red, fontSize: 14),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    show(context, "Link Clicked.");
+                  },
+              ),
+              ImageSpan(
+                AssetImage("static/gsy_cat.png"),
                 imageWidth: 24,
                 imageHeight: 24,
-                margin: EdgeInsets.symmetric(horizontal: 10)),
-            TextSpan(
-              text: "哈哈哈",
-              style: TextStyle(color: Colors.yellow, fontSize: 14),
-            ),
-            TextSpan(
-              text: "@Somebody",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () {
-                  debugPrint("Link Clicked");
-                },
-            ),
-            TextSpan(
-              text: " #RealRichText# ",
-              style: TextStyle(color: Colors.blue, fontSize: 14),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () {
-                  debugPrint("Link Clicked");
-                },
-            ),
-            TextSpan(
-              text: "showing a bigger image",
-              style: TextStyle(color: Colors.black, fontSize: 14),
-            ),
-            ImageSpan(AssetImage("static/gsy_cat.png"),
-                imageWidth: 24,
-                imageHeight: 24,
-                margin: EdgeInsets.symmetric(horizontal: 5)),
-            TextSpan(
-              text: "and seems working perfect……",
-              style: TextStyle(color: Colors.black, fontSize: 14),
-            ),
-          ]),
-        ),
+              ),
+              ImageSpan(AssetImage("static/gsy_cat.png"),
+                  imageWidth: 24,
+                  imageHeight: 24,
+                  margin: EdgeInsets.symmetric(horizontal: 10)),
+              TextSpan(
+                text: "哈哈哈",
+                style: TextStyle(color: Colors.yellow, fontSize: 14),
+              ),
+              TextSpan(
+                text: "@Somebody",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    show(context, "Link Clicked.");
+                  },
+              ),
+              TextSpan(
+                text: " #RealRichText# ",
+                style: TextStyle(color: Colors.blue, fontSize: 14),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    show(context, "Link Clicked.");
+                  },
+              ),
+              TextSpan(
+                text: "showing a bigger image",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              ImageSpan(AssetImage("static/gsy_cat.png"),
+                  imageWidth: 24,
+                  imageHeight: 24,
+                  margin: EdgeInsets.symmetric(horizontal: 5)),
+              TextSpan(
+                text: "and seems working perfect……",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+            ]),
+          );
+        }),
       ),
     );
+  }
+
+  show(context, text) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(text),
+      action: SnackBarAction(
+        label: 'ACTION',
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('You pressed snackbar\'s action.'),
+          ));
+        },
+      ),
+    ));
   }
 }
