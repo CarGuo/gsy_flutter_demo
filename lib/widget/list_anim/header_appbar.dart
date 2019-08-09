d014977df6b821420778b0a32345146d8bdd447e
guoshuyu <359369982@qq.com>
2019-08-09 15:24:54 +0800

    update demo

--- lib/widget/list_anim/header_appbar.dart
+++ lib/widget/list_anim/header_appbar.dart
@@ -1,59 +1,94 @@
 import 'package:flutter/material.dart';
 
 class HeaderAppBar extends StatelessWidget {
-  final int alpha;
+  final int alphaBg;
+  final bool showStickItem;
 
-  HeaderAppBar({this.alpha = 0});
+  HeaderAppBar({this.alphaBg = 0, this.showStickItem = false});
 
   @override
   Widget build(BuildContext context) {
-
-    double paddingTop =
+    double statusBarHeight =
         MediaQueryData.fromWindow(WidgetsBinding.instance.window).padding.top;
 
-    var color = Theme.of(context).primaryColor.withAlpha(alpha);
+    double reactHeight = 30;
+
+    ///总高度 = appbar 高度 +  statusBar 高度 + 底部停靠区域高度
+    double containerHeight = kToolbarHeight + statusBarHeight + reactHeight;
+
+    var color = Theme.of(context).primaryColor.withAlpha(alphaBg);
 
     return new Material(
       color: Colors.transparent,
       child: new Container(
         alignment: Alignment.centerLeft,
-        height: kToolbarHeight + paddingTop,
-        color: color,
-        child: new Container(
-          height: kToolbarHeight,
-          margin: EdgeInsets.only(top: paddingTop),
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Container(
-                width: 36,
-                height: 36,
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(right: 10, left: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white.withAlpha(125),
-                    borderRadius: BorderRadius.all(Radius.circular(18))),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
+        height: containerHeight,
+        child: new Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            ///撑满状态栏颜色
+            new Container(
+              height: statusBarHeight,
+              color: color,
+            ),
+            new Container(
+              color: color,
+              height: kToolbarHeight,
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Container(
+                    width: 36,
+                    height: 36,
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withAlpha(125),
+                        borderRadius: BorderRadius.all(Radius.circular(18))),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
+                  new Expanded(
+                    child: new Container(
+                      height: kToolbarHeight - 15,
+                      margin: EdgeInsets.only(right: 20, left: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white.withAlpha(125),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                    ),
+                  ),
+                ],
               ),
-              new Expanded(
-                child: new Container(
-                  height: kToolbarHeight - 15,
-                  margin: EdgeInsets.only(right: 20, left: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white.withAlpha(125),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                ),
-              ),
-            ],
-          ),
+            ),
+            showStickItem
+                ? Container(
+                    alignment: Alignment.centerLeft,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(left: 10),
+                    height: reactHeight,
+                    color: Colors.amber,
+                    child: new Row(
+                      children: <Widget>[
+                        new Icon(Icons.ac_unit, color: Colors.white, size: 13),
+                        new SizedBox(
+                          width: 10,
+                        ),
+                        new Text(
+                          "StickText",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
+                : new Container()
+          ],
         ),
       ),
     );
--- lib/widget/list_anim/list_anim_demo_page.dart
+++ lib/widget/list_anim/list_anim_demo_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:gsy_flutter_demo/widget/list_anim/header_appbar.dart';
 
@@ -7,45 +9,103 @@ class ListAnimDemoPage extends StatefulWidget {
 }
 
 class _ListAnimDemoPageState extends State<ListAnimDemoPage> {
-  int alpha = 0;
+  ///AppBar 的背景色透明度
+  int appBarColorAlpha = 0;
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  ///记录滚动距离
+  double scrollPix = 0;
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  ///是否需要显示停靠
+  bool showStickItem = false;
 
+  ///头部区域高度
+  double headerHeight = 300;
+
+  ///头部区域偏离图片高度
+  double headerRectMargin = 40;
+
+  ///头部信息框高度
+  double headerRectHeight = 60;
+
+
+  ///头部区域
   _buildHeader() {
-    double height = 200;
+    ///状态栏高度
+    double statusBarHeight =
+        MediaQueryData.fromWindow(WidgetsBinding.instance.window).padding.top;
+    ///头部区域去除marin、appbar、状态栏之后的高度
+    double dynamicValue =
+        headerHeight - headerRectMargin - kToolbarHeight - statusBarHeight;
+
+    ///计算 margin 的撑开动画效果，用于视觉偏差
+    ///计算停靠 item 的显示标志 showStickItem
+    double marginEdge = 0;
+    if (scrollPix >= dynamicValue) {
+      marginEdge = 10 - (scrollPix - dynamicValue);
+      marginEdge = math.max(0, marginEdge);
+      if (marginEdge == 0) {
+        showStickItem = true;
+      } else {
+        showStickItem = false;
+      }
+    } else {
+      marginEdge = 10;
+      showStickItem = false;
+    }
+
     return Container(
       alignment: Alignment.topCenter,
-      height: height,
-      child: new Image.asset(
-        "static/gsy_cat.png",
-        fit: BoxFit.cover,
-        width: MediaQuery.of(context).size.width,
-        height: height,
+      height: headerHeight,
+      child: new Stack(
+        children: <Widget>[
+          new Image.asset(
+            "static/gsy_cat.png",
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: headerHeight - headerRectMargin,
+          ),
+          new Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: headerRectHeight,
+              color: Colors.amber,
+              margin: EdgeInsets.only(left: marginEdge, right: marginEdge),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: new Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  new Text(
+                    "StickText",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  new Expanded(child: new Container()),
+                  new Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
 
+  ///处理滑动监听
   _handleScrollUpdateNotification(ScrollUpdateNotification notification) {
-    print("${notification.metrics.pixels}");
+    scrollPix = notification.metrics.pixels;
     var curAlpha = 0;
     if (notification.metrics.pixels <= 0) {
       curAlpha = 0;
     } else {
-      curAlpha = ((notification.metrics.pixels / 200) * 255).toInt();
+      curAlpha = ((notification.metrics.pixels / 180) * 255).toInt();
       if (curAlpha > 255) {
         curAlpha = 255;
       }
     }
     setState(() {
-      alpha = curAlpha;
+      appBarColorAlpha = curAlpha;
     });
   }
 
@@ -54,6 +114,7 @@ class _ListAnimDemoPageState extends State<ListAnimDemoPage> {
     return new Stack(
       children: <Widget>[
         Scaffold(
+          ///去除 SafeArea 的 padding
           body: MediaQuery.removePadding(
             context: context,
             removeLeft: true,
@@ -89,10 +150,10 @@ class _ListAnimDemoPageState extends State<ListAnimDemoPage> {
           ),
         ),
         HeaderAppBar(
-          alpha: alpha,
+          alphaBg: appBarColorAlpha,
+          showStickItem: showStickItem,
         ),
       ],
     );
   }
 }
-
