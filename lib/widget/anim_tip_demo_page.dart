7a51a6a5c5f94015c3271387140034f5d55a9f31
guoshuyu <359369982@qq.com>
2019-09-10 18:23:48 +0800

    update anim

--- lib/widget/anim_tip_demo_page.dart
+++ lib/widget/anim_tip_demo_page.dart
@@ -16,45 +16,43 @@ class _AnimTipDemoPageState extends State<AnimTipDemoPage> {
       ),
       body: Container(
         child: new Column(children: [
-          new Expanded(
-            child: Container(
-              child:  AnimatedSwitcher(
-                switchInCurve: Cubic(0.4, 0.0, 0.2, 1.0),
-                switchOutCurve: Cubic(1.0, 0.1, 1.0, 0.1),
-                transitionBuilder: (child, anim) {
-                  return SlideTransition(
-                      child: child,
-                      position: Tween<Offset>(
-                        begin: Offset(0.0, -0.5),
-                        end: Offset(0.0, 0.0),
-                      ).animate(anim));
-                },
-                duration: Duration(milliseconds: 500),
-                child: showTipItem
-                    ? Container(
-                  alignment: Alignment.centerLeft,
-                  width: MediaQuery.of(context).size.width,
-                  height: 70,
-                  key: ValueKey("TipItem"),
-                  color: Colors.amber,
-                  child: new Row(
-                    children: <Widget>[
-                      new Icon(Icons.ac_unit,
-                          color: Colors.white, size: 13),
-                      new SizedBox(
-                        width: 10,
+          Container(
+            child: AnimatedSwitcher(
+              switchInCurve: Cubic(0.4, 0.0, 0.2, 1.0),
+              switchOutCurve: Cubic(1.0, 0.1, 1.0, 0.1),
+              transitionBuilder: (child, anim) {
+                return SlideTransition(
+                    child: child,
+                    position: Tween<Offset>(
+                      begin: Offset(0.0, -1.0),
+                      end: Offset(0.0, 0.0),
+                    ).animate(anim));
+              },
+              duration: Duration(milliseconds: 500),
+              child: showTipItem
+                  ? Container(
+                      alignment: Alignment.centerLeft,
+                      width: MediaQuery.of(context).size.width,
+                      height: 70,
+                      key: ValueKey("TipItem"),
+                      color: Colors.amber,
+                      child: new Row(
+                        children: <Widget>[
+                          new Icon(Icons.ac_unit,
+                              color: Colors.white, size: 13),
+                          new SizedBox(
+                            width: 10,
+                          ),
+                          new Text(
+                            "StickText",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
                       ),
-                      new Text(
-                        "StickText",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                )
-                    : new Container(
-                  key: ValueKey("hideItem"),
-                ),
-              ),
+                    )
+                  : new Container(
+                      key: ValueKey("hideItem"),
+                    ),
             ),
           ),
           new Expanded(
