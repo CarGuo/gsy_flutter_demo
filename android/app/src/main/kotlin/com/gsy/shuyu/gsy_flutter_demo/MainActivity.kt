fd51733556e8ff4cb00ef83822472f7edfa63d2a
Shuyu Guo <359369982@qq.com>
2020-10-11 20:28:50 +0800

    update support 1.22.1

--- android/app/src/main/kotlin/com/gsy/shuyu/gsy_flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/gsy/shuyu/gsy_flutter_demo/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.gsy.shuyu.gsy_flutter_demo
 
 import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
