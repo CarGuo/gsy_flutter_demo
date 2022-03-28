e43c288a61533ba681d767cccdbfc1e8ec8150b6
guoshuyu <359369982@qq.com>
2022-03-28 10:49:41 +0800

    update

--- lib/widget/gradient_text_demo_page.dart
+++ lib/widget/gradient_text_demo_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class GradientTextDemoPage extends StatelessWidget {
@@ -12,32 +15,59 @@ class GradientTextDemoPage extends StatelessWidget {
       body: Container(
         child: Center(
           child: Stack(
+            alignment: Alignment.center,
             children: [
-              Text(
-                '8',
-                style: TextStyle(
-                    fontSize: 100,
-                    foreground: Paint()
-                      ..style = PaintingStyle.fill
-                      ..strokeWidth = 3
-                      ..shader = LinearGradient(
-                              begin: Alignment.bottomLeft,
-                              end: Alignment.topRight,
-                              colors: [Colors.yellow, Colors.black])
-                          .createShader(Rect.fromLTWH(0, 0, 200, 100))),
+              if (kIsWeb)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text("当前效果不支持 Web ，请在 App 查看"),
+                  ),
+                ),
+              Container(
+                child: Text(
+                  '8',
+                  style: TextStyle(
+                      fontSize: 100,
+
+                      /// 2.10 下因为有 shader （Gradient） ， web 下会用 canvas
+                      ///编译文本，此时会有  _applySpanStyleToCanvas 时 setUpPaint 的 Rect 为 nul 的问题
+                      ///所以添加   fontFeatures 可以在底层渲染时切换回 p+span 标签
+                      ///但是目前 p+span 不支持 foreground 的 Paint
+                      fontFeatures:
+                          kIsWeb ? [FontFeature.enable("tnum")] : null,
+                      foreground: Paint()
+                        ..style = PaintingStyle.fill
+                        ..strokeWidth = 3
+                        ..shader = LinearGradient(
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight,
+                                colors: [Colors.yellow, Colors.black])
+                            .createShader(Rect.fromLTWH(0, 0, 200, 100))),
+                ),
               ),
-              Text(
-                '8',
-                style: TextStyle(
-                    fontSize: 100,
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 2
-                      ..shader = LinearGradient(
-                              begin: Alignment.bottomLeft,
-                              end: Alignment.topRight,
-                              colors: [Colors.limeAccent, Colors.cyanAccent])
-                          .createShader(Rect.fromLTWH(0, 0, 200, 100))),
+              Container(
+                child: Text(
+                  '8',
+                  style: TextStyle(
+                      fontSize: 100,
+
+                      /// 2.10 下因为有 shader （Gradient） ， web 下会用 canvas
+                      ///编译文本，此时会有  _applySpanStyleToCanvas 时 setUpPaint 的 Rect 为 nul 的问题
+                      ///所以添加   fontFeatures 可以在底层渲染时切换回 p+span 标签
+                      ///但是目前 p+span 不支持 foreground 的 Paint
+                      fontFeatures:
+                          kIsWeb ? [FontFeature.enable("tnum")] : null,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 2
+                        ..shader = LinearGradient(
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight,
+                                colors: [Colors.limeAccent, Colors.cyanAccent])
+                            .createShader(Rect.fromLTWH(0, 0, 200, 100))),
+                ),
               ),
             ],
           ),
