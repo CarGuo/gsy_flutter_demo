5055a63bb7b62f8feac5c2994112159a7196b37c
guoshuyu <359369982@qq.com>
2022-07-22 10:55:21 +0800

    add demo

--- lib/widget/card_perspective_demo_page.dart
+++ lib/widget/card_perspective_demo_page.dart
@@ -17,27 +17,7 @@ class _CardPerspectiveDemoPageState extends State<CardPerspectiveDemoPage> {
   double cardHeight = 400;
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  double getCardAngleX(double samplingTouchY) {
-    return startTransform ? (1.2 * (samplingTouchY / 50) + -1.2) : 0;
-  }
-
-  double getCardAngleY(double samplingTouchX) {
-    return startTransform ? (-0.3 * (samplingTouchX / 50) + 0.3) : 0;
-  }
-
-  double getMemberTranslate(double samplingTouch, keyValue) {
-    return startTransform ? (keyValue * (samplingTouch / 50) - keyValue) : 0.0;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double samplingAngleX = touchX / cardWidth * 80;
-    double samplingAngleY = touchY / cardHeight / 2 * 100;
-
     return Scaffold(
       appBar: AppBar(title: Text("CardPerspectiveDemoPage")),
       body: Container(
@@ -50,10 +30,9 @@ class _CardPerspectiveDemoPageState extends State<CardPerspectiveDemoPage> {
         ),
         child: Transform(
           transform: Matrix4.identity()
-            ///调整视角，第3行第2列，也就是调整 z 轴的视角，产生透视偏差效果
             ..setEntry(3, 2, 0.001)
-            ..rotateX(getCardAngleX(samplingAngleY))
-            ..rotateY(getCardAngleY(samplingAngleX)),
+            ..rotateX(startTransform ? touchY : 0.0)
+            ..rotateY(startTransform ? touchX : 0.0),
           alignment: FractionalOffset.center,
           child: GestureDetector(
             onTapUp: (_) => setState(() {
@@ -66,16 +45,16 @@ class _CardPerspectiveDemoPageState extends State<CardPerspectiveDemoPage> {
             onPanUpdate: (details) {
               setState(() => startTransform = true);
 
-              ///x轴限制范围
-              if (details.localPosition.dx < cardWidth &&
+              ///y轴限制范围
+              if (details.localPosition.dx < cardWidth * 0.55 &&
                   details.localPosition.dx > cardWidth * 0.3) {
-                touchX = details.localPosition.dx;
+                touchX = (cardWidth / 2 - details.localPosition.dx) / 100;
               }
 
-              ///y轴限制范围
-              if (details.localPosition.dy > cardHeight * 0.5 &&
-                  details.localPosition.dy < cardHeight * 1.4) {
-                touchY = details.localPosition.dy;
+              ///x轴限制范围
+              if (details.localPosition.dy > cardHeight * 0.4 &&
+                  details.localPosition.dy < cardHeight * 0.6) {
+                touchY = (details.localPosition.dy - cardHeight / 2) / 100;
               }
             },
             child: Container(
@@ -83,55 +62,49 @@ class _CardPerspectiveDemoPageState extends State<CardPerspectiveDemoPage> {
               height: cardHeight,
               padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                      colors: [Colors.white, Colors.blueGrey],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x70000000),
-                      blurRadius: 30,
-                      spreadRadius: -30,
-                      offset: Offset(0, 60),
-                    )
-                  ]),
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    colors: [Colors.white, Colors.blueGrey],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 25,
+                    spreadRadius: -25,
+                    offset: Offset(0, 30),
+                  )
+                ],
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Transform(
                     transform: Matrix4.identity()
-                      ..translate(getMemberTranslate(samplingAngleX, 20),
-                          getMemberTranslate(samplingAngleY, 20), 0.0),
+                      ..translate(startTransform ? touchX * 50 - 5 : 0.0,
+                          startTransform ? touchY * 50 - 5 : 0.0, 0.0),
                     alignment: FractionalOffset.center,
-                    child: Container(
-                      child: Text(
-                        '13',
-                        style: TextStyle(
-                          fontSize: 80,
-                          fontFamily: 'Canterbury',
-                          fontWeight: FontWeight.w800,
-                          shadows: [
-                            Shadow(
-                                color: Colors.grey,
-                                blurRadius: 5,
-                                offset: Offset(
-                                    startTransform
-                                        ? samplingAngleY / 10 - 5
-                                        : 0,
-                                    startTransform
-                                        ? samplingAngleX / 10 - 5
-                                        : 0)),
-                          ],
-                          foreground: Paint()
-                            ..style = PaintingStyle.fill
-                            ..strokeWidth = 3
-                            ..shader = LinearGradient(
-                                    begin: Alignment.bottomLeft,
-                                    end: Alignment.topRight,
-                                    colors: [Colors.yellow, Colors.black])
-                                .createShader(Rect.fromLTWH(0, 0, 200, 100)),
-                        ),
+                    child: Text(
+                      '13',
+                      style: TextStyle(
+                        fontSize: 80,
+                        fontFamily: 'Canterbury',
+                        fontWeight: FontWeight.w800,
+                        shadows: [
+                          Shadow(
+                              color: Colors.black54,
+                              blurRadius: 2,
+                              offset: Offset(1, 1)),
+                        ],
+                        foreground: Paint()
+                          ..style = PaintingStyle.fill
+                          ..strokeWidth = 3
+                          ..shader = LinearGradient(
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topRight,
+                                  colors: [Colors.yellow, Colors.black])
+                              .createShader(Rect.fromLTWH(0, 0, 200, 100)),
                       ),
                     ),
                   ),
@@ -160,8 +133,8 @@ class _CardPerspectiveDemoPageState extends State<CardPerspectiveDemoPage> {
                   ),
                   Transform(
                     transform: Matrix4.identity()
-                      ..translate(getMemberTranslate(samplingAngleX, 15),
-                          getMemberTranslate(samplingAngleY, 15), 0.0),
+                      ..translate(startTransform ? touchX * 30 - 5 : 0.0,
+                          startTransform ? touchY * 30 - 5 : 0.0, 0.0),
                     alignment: FractionalOffset.center,
                     child: Image.asset(
                       "static/gsy_cat.png",
