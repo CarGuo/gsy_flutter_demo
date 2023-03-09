212d10e627d7a8adb1b85c6f88c9a90802fbd63e
guoshuyu <359369982@qq.com>
2023-03-09 09:33:19 +0800

    update demo

--- lib/widget/async_to_sync_call_page.dart
+++ lib/widget/async_to_sync_call_page.dart
@@ -14,13 +14,29 @@ class _AsyncToSyncCallPageState extends State<AsyncToSyncCallPage> {
 
   Future<void> _syncWait(Future Function() fn) async {
     var currentAsync = _syncCompleter;
-    var completer = Completer<void>();
+    final completer = Completer<void>();
     _syncCompleter = completer;
 
     ///第一次是 null
     await currentAsync?.future;
     await fn.call();
     completer.complete();
+  }
+
+  ///因为 setState 不是同步的，只是内部把标记位标志为脏数据
+  ///所以如果需要等待 setState 执行结束，需要做一个等待
+  waitSetStateComplete(Future Function() fn) async {
+    await fn();
+    if (mounted) {
+      setState(() {});
+    }
+    final completer = Completer<void>();
+
+    ///下一帧结束
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      completer.complete();
+    });
+    return completer.future;
   }
 
   @override
